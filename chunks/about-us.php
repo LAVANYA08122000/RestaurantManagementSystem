19e5ec7c5036e4d53ecbde5fa2a55f95558e225e
LAVANYA08122000 <66677606+LAVANYA08122000@users.noreply.github.com>
2021-08-17 17:02:23 +0530

    Add files via upload

---
+++ chunks/about-us.php
@@ -0,0 +1,15 @@
+<section class="fabout">
+		<div class="section white center">
+		      <div class="row container">
+		        <h2 class="header">ABOUT US</h2>
+		        <p class="grey-text text-darken-3 lighten-3">'LAVANYA Restro’s is additionally home of the amazing Table-Side Caesar Spun Signature Salad, specially made table side from a rundown of 10 fixings including blue cheddar, shrimp, olives, banana peppers, anchovies and our homemade mystery Caesar dressing. Other house top choices are our Grilled or Chilled Shrimp, new walleye conveyed every day from Red Lake Nation in Minnesota and house ground steak burgers.
+
+Our menu offers something for everybody, from light passage of plates of mixed greens and little nibbles to the inconceivable  T Bone steak. Our steaks are sliced in house to guarantee flawlessness and expertly barbecued at 2,000 degrees.
+
+We will probably give excellent client benefit, the best quality fixings, and a noteworthy affair whether it’s an uncommon occasion, easygoing social event of companions or a sentimental night out on the town.
+
+‘LAVANYA Restro’s’ is situated in downtown Wayzata, straightforwardly opposite lofty Lake Minnetonka. Extraordinary administration and a fun vibe keep local people returning and guests raving..
+
+</p>
+		      </div>
+	</section>
